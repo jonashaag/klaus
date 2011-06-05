62fb7157aafd03149566e3e7d8edc9432b9cbc1f
Jonas Haag <jonas@lophus.org>
2011-06-05 11:16:48 -0700

    dumbtest: Track server speed, refactoring

--- dumbtest.py
+++ dumbtest.py
@@ -1,38 +1,54 @@
 """ Very dumb testing tool: Ensures all sites respond with HTTP 2xx/3xx """
-from __future__ import print_function
 import sys
 import re
+import time
 import httplib
 from collections import defaultdict
 import atexit
+
+def view_from_url(url):
+    try:
+        return url.split('/')[2]
+    except IndexError:
+        return url
 
 AHREF_RE = re.compile('href="([\w/][^"]+)"')
 
 BASE_URL = 'http://localhost:8080'
 
 errors = defaultdict(set)
-atexit.register(lambda: print(errors))
+durations = defaultdict(list)
 
-urls = {'/'}
-seen = set()
-while urls:
-    try:
-        http_conn.close()
-    except NameError:
-        pass
-    http_conn = httplib.HTTPConnection('localhost', 8080)
-    url = urls.pop()
-    if url in seen:
-        continue
-    seen.add(url)
-    if '-v' in sys.argv:
-        print('Requesting %r...' % url)
-    http_conn.request('GET', BASE_URL + url)
-    response = http_conn.getresponse()
-    status = str(response.status)
-    if status[0] == '3':
-        urls.add(response.getheader('Location'))
-    elif status[0] == '2':
-        urls.update(AHREF_RE.findall(response.read()))
-    else:
-        errors[status].add(url)
+def main():
+    urls = {'/'}
+    seen = set()
+    while urls:
+        try:
+            http_conn.close()
+        except NameError:
+            pass
+        http_conn = httplib.HTTPConnection('localhost', 8080)
+        url = urls.pop()
+        if url in seen:
+            continue
+        seen.add(url)
+        if '-v' in sys.argv:
+            print 'Requesting %r...' % url
+        start = time.time()
+        http_conn.request('GET', BASE_URL + url)
+        response = http_conn.getresponse()
+        durations[view_from_url(url)].append(time.time() - start)
+        status = str(response.status)
+        if status[0] == '3':
+            urls.add(response.getheader('Location'))
+        elif status[0] == '2':
+            urls.update(AHREF_RE.findall(response.read()))
+        else:
+            errors[status].add(url)
+
+def print_stats():
+    print errors
+    print {url: sum(times)/len(times) for url, times in durations.iteritems()}
+atexit.register(print_stats)
+
+main()
