8b6b20e1b9984ade47433504a96edd3016019b23
posativ <info@posativ.org>
2012-04-25 19:23:59 +0200

    install data_files to egg

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+# this is a clever hack to circumvent distutil's data_files
+# policy "install once, find never". Definitely a TODO!
+# -- https://groups.google.com/group/comp.lang.python/msg/2105ee4d9e8042cb
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
 from distutils.core import setup
+from os.path import join
+
+templates = ['base.html', 'history.html', 'repo_list.html', 'skeleton.html',
+             'tree.inc.html', 'view_blob.html', 'view_commit.html']
+static = ['klaus.css', 'line-highlighter.js', 'pygments.css']
 
 setup(
     name='klaus',
-    version='1.0.0',
+    version='1.0.1',
     author='Jonas Haag',
     author_email='jonas@lophus.org',
-    packages=['klaus', 'klaus/templates', 'klaus/static'],
+    packages=['klaus'],
     scripts=['bin/klaus'],
-    package_data={
-        'klaus': ['templates/*.html', 'static/*']
-    },
+    data_files=[
+        ('klaus/templates', [join('klaus/templates', path) for path in templates]),
+        ('klaus/static', [join('klaus/static', path) for path in static])
+    ],
     url='https://github.com/jonashaag/klaus',
     license='BSD style',
     description='The first Git web viewer that Just Works™.',
@@ -20,7 +33,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
