9c84806fd2a86525144ccd6d36474874657627b6
Jonas Haag <jonas@lophus.org>
2013-06-10 23:47:28 +0200

    Fix #56: Enable 'toc' and 'extra' markdown renderer extensions
    Thanks to @ar4s for the initial patch!

--- klaus/markup.py
+++ klaus/markup.py
@@ -2,27 +2,6 @@ import os
 
 LANGUAGES = []
 
-try:
-    import markdown
-    LANGUAGES.append((['.md', '.mkdn'], markdown.markdown))
-except ImportError:
-    pass
-
-try:
-    from docutils.core import publish_parts
-    from docutils.writers.html4css1 import Writer
-
-    def render_rest(content):
-        # start by h2 and ignore invalid directives and so on (most likely from Sphinx)
-        settings = {'initial_header_level': 2, 'report_level': 'quiet'}
-        return publish_parts(content,
-                             writer=Writer(),
-                             settings_overrides=settings).get('html_body')
-
-    LANGUAGES.append((['.rst', '.rest'], render_rest))
-except ImportError:
-    pass
-
 
 def get_renderer(filename):
     _, ext = os.path.splitext(filename)
@@ -40,3 +19,37 @@ def render(filename, content=None):
         content = open(filename).read()
 
     return get_renderer(filename)(content)
+
+
+def _load_markdown():
+    try:
+        import markdown
+    except ImportError:
+        return
+
+    def render_markdown(content):
+        return markdown.markdown(content, extensions=['toc', 'extra'])
+
+    LANGUAGES.append((['.md', '.mkdn'], render_markdown))
+
+
+def _load_restructured_text():
+    try:
+        from docutils.core import publish_parts
+        from docutils.writers.html4css1 import Writer
+    except ImportError:
+        return
+
+    def render_rest(content):
+        # start by h2 and ignore invalid directives and so on (most likely from Sphinx)
+        settings = {'initial_header_level': 2, 'report_level': 'quiet'}
+        return publish_parts(content,
+                            writer=Writer(),
+                            settings_overrides=settings).get('html_body')
+
+    LANGUAGES.append((['.rst', '.rest'], render_rest))
+
+
+
+for loader in [_load_markdown, _load_restructured_text]:
+    loader()
