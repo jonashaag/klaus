a6e3660cf867b16d090cd767c381b3087f97b5c8
Jonas Haag <jonas@lophus.org>
2015-06-24 13:29:16 +0200

    Add some tests for 'klaus.make_app'

---
+++ runtests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+( rm -rf tests/repos/test_repo
+  mkdir -p tests/repos/test_repo
+  cd tests/repos/test_repo
+  git init
+  echo Hello World > README
+  git add README
+  git commit -a -m "First commit"
+)
+
+py.test tests/
