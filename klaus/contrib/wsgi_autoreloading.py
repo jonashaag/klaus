6798f9bd448085b6a803566de5ae3397995eb58e
Jonas Haag <jonas@lophus.org>
2019-07-06 13:09:17 +0200

    Rename variable

--- klaus/contrib/wsgi_autoreloading.py
+++ klaus/contrib/wsgi_autoreloading.py
@@ -26,24 +26,24 @@ def poll_for_changes(interval, dir):
     old_contents = glob.glob(glob_pattern)
     while 1:
         time.sleep(interval)
-        if _.should_reload:
+        if S.should_reload:
             # klaus application has not seen our change yet
             continue
         new_contents = glob.glob(glob_pattern)
         if new_contents != old_contents:
             # Directory contents changed => should_reload
             old_contents = new_contents
-            _.should_reload = True
+            S.should_reload = True
 
 
 def make_autoreloading_app(repos_root, *args, **kwargs):
     def app(environ, start_response):
-        if _.should_reload:
+        if S.should_reload:
             # Refresh inner application with new repo list
             print("Reloading repository list...")
-            _.inner_app = make_app(glob.glob(repos_root + '/*'), *args, **kwargs)
-            _.should_reload = False
-        return _.inner_app(environ, start_response)
+            S.inner_app = make_app(glob.glob(repos_root + '/*'), *args, **kwargs)
+            S.should_reload = False
+        return S.inner_app(environ, start_response)
 
     # Background thread that polls the directory for changes
     poller_thread = threading.Thread(target=(lambda: poll_for_changes(10, repos_root)))
