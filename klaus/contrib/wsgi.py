4dbce3f75a317eac65682be4568279455592cb02
Andrei Onel <onel@users.noreply.github.com>
2025-07-29 02:32:20 +0300

    Added reference documentation for: klaus/contrib/wsgi.py

--- klaus/contrib/wsgi.py
+++ klaus/contrib/wsgi.py
@@ -1,9 +1,18 @@
+"""
+WSGI application factory module for Klaus.
+
+This module creates a WSGI application instance by reading configuration
+from environment variables and setting up optional HTTP digest authentication.
+"""
+
 from klaus import make_app
 
 from .app_args import get_args_from_env
 
+# Get application arguments and keyword arguments from environment variables
 args, kwargs = get_args_from_env()
 
+# Handle optional HTTP digest authentication file
 if kwargs["htdigest_file"]:
     with open(kwargs["htdigest_file"]) as file:
         kwargs["htdigest_file"] = file
